5ded21c280bbd17674c2f8440e06ab584bc8653d
nazarPtyt <ockap90@libero.it>
2023-04-12 20:43:31 +0200

    feat(Contact): add Get In Touch block with alerted data & validation

--- src/pages/Contact/components/ContactInformation/ContactInformation.tsx
+++ src/pages/Contact/components/ContactInformation/ContactInformation.tsx
@@ -1,32 +1,56 @@
 import React from 'react'
 
 import PhoneForwardedRoundedIcon from '@mui/icons-material/PhoneForwardedRounded'
+import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined'
+import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 
 import s from './ContactInformation.module.scss'
 export const ContactInformation = () => {
   return (
-    <div className={s.wrapper}>
+    <address className={s.wrapper}>
       <div>
-        <h5>Contact Information</h5>
+        <h2>Contact Information</h2>
         <p>
           Fill the form below or write us .We will help you as soon as possible.
         </p>
       </div>
-      <div>
-        <div>
+      <div className={s.inner}>
+        <Box
+          bgcolor={(theme) => theme.palette.secondary.contrastText}
+          className={s.item}>
           <PhoneForwardedRoundedIcon />
           <h3>Phone</h3>
           <Link href="tel:+32398473847383">+(323) 9847 3847 383</Link>
           <Link href="tel:+43454665467443">+(434) 5466 5467 443</Link>
-        </div>
-        <div>
+        </Box>
+        <Box
+          bgcolor={(theme) => theme.palette.secondary.contrastText}
+          className={s.item}>
           <PhoneForwardedRoundedIcon />
           <h3>Email</h3>
-          <Link href="tel:+32398473847383">+(323) 9847 3847 383</Link>
-          <Link href="tel:+43454665467443">+(434) 5466 5467 443</Link>
-        </div>
+          <Link href="mailto:Demoemail@gmail.com">Demoemail@gmail.com</Link>
+          <Link href="mailto:itstore@gmail.com">itstore@gmail.com</Link>
+        </Box>
+        <Box
+          className={s.addressBox}
+          bgcolor={(theme) => theme.palette.secondary.contrastText}>
+          <div>
+            <RoomOutlinedIcon />
+            <span>
+              <h3>Address</h3>
+              <p>4517 Washington Ave. Manchester, Road 2342, Kentucky 39495</p>
+            </span>
+          </div>
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d774382.6761693481!2d-73.979681!3d40.69748800000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1681319296529!5m2!1sen!2sbd"
+              className={s.map}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </Box>
       </div>
-    </div>
+    </address>
   )
 }
--- src/pages/Contact/components/GetInTouch/GetInTouch.tsx
+++ src/pages/Contact/components/GetInTouch/GetInTouch.tsx
@@ -1,6 +1,76 @@
 import React from 'react'
 
+import { yupResolver } from '@hookform/resolvers/yup'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { string } from 'yup'
+
 import s from './GetInTouch.module.scss'
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    email: string().email('Invalid email format').required('Mail is required'),
+    subject: string().required(),
+    message: string().min(10).required(),
+  })
+  .required()
+
+type FormData = yup.InferType<typeof schema>
+
 export const GetInTouch = () => {
-  return <div className={s.wrapper}></div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<FormData> = (data) =>
+    alert(JSON.stringify(data, null, 2))
+
+  return (
+    <div className={s.wrapper}>
+      <h2>Get In Touch</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          placeholder={'First Name*'}
+          style={{ width: '80%' }}
+          label={'First Name'}
+          {...register('firstName', { required: true })}
+        />
+        <p>{errors.firstName?.message}</p>
+        <TextField
+          label={'Email Address'}
+          placeholder={'Email Address*'}
+          type={'email'}
+          style={{ width: '80%' }}
+          {...register('email', { required: true })}
+        />
+        <p>{errors.email?.message}</p>
+        <TextField
+          placeholder={'Subject*'}
+          style={{ width: '80%' }}
+          label={'Subject'}
+          {...register('subject', { required: true })}
+        />
+        <p>{errors.subject?.message}</p>
+        <TextField
+          placeholder={'Message*'}
+          label={'Message'}
+          multiline
+          maxRows={6}
+          style={{ width: '80%' }}
+          {...register('message', { required: true })}
+        />
+        <p>{errors.message?.message}</p>
+        <Button type="submit" variant={'contained'}>
+          Send Now
+        </Button>
+      </form>
+    </div>
+  )
 }
