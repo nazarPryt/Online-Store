3ed66dab15c27a434d2812644726001c40d1a0a4
nazarPtyt <ockap90@libero.it>
2023-11-10 14:44:34 +0100

    style: add Track Order page

--- src/pages/TrackOrder/TrackOrderPage.tsx
+++ src/pages/TrackOrder/TrackOrderPage.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import TextField from '@mui/material/TextField'
 
+import deliveryImg from '../../assets/delivery.png'
 import { SectionMui } from '../../components/SectionMui/SectionMui'
 
 import s from './TrackOrderPage.module.scss'
@@ -14,15 +15,22 @@ export const TrackOrderPage = () => {
       <Container>
         <h2>Track Your Order</h2>
         <p>Enter your order taracking number and your secreet id.</p>
-        <div>
+        <div className={s.wrapper}>
           <form>
-            <label htmlFor="orderNumber" />
+            <label htmlFor="orderNumber">Order Tracking Number*</label>
             <TextField id={'orderNumber'} placeholder={'Order Number'} />
 
-            <label htmlFor="deliveryDate" />
-            <TextField id={'deliveryDate'} placeholder={'Delivery Date'} />
+            <label htmlFor="deliveryDate">Delivery Date</label>
+            <TextField
+              id={'deliveryDate'}
+              placeholder={'Delivery Date'}
+              type={'date'}
+            />
             <Button>Truck Now</Button>
           </form>
+          <div className={s.deliveryImgBox}>
+            <img src={deliveryImg} alt="deliveryImg" />
+          </div>
         </div>
       </Container>
     </SectionMui>
